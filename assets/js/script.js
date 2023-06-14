1139f74d7a978bd8ad800b58a466d09f02608197
BeliciaH <bea.monkey18@gmail.com>
2023-06-14 19:49:26 -0400

    Woohoo the branch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,11 @@
 //js for project 1
 //test
-<<<<<<< HEAD
+
 var requestUrl = "https://syndication.api.eb.com/production/article/1/xml";
 var requestUrl2 =
   "https://images-api.nasa.gov/&appid=kSgH3akh7Qaqauv7AbKZyRc6pQjxcfVEOlObfDDl";
 var requestUrl3 =
-  "https://syndication.api.eb.com/production/articles?articleTypeId=31&lastUpdated=2020-10-01";
-var requestOptions = {
-  method: "GET",
-  headers: {
-    "x-api-key": "3eec413a-3631-4d4d-9dc0-1cb1ecd69be5",
-    accept: "application/JSON",
-  },
-};
-fetch(requestUrl3, requestOptions).then(function (response) {
-  console.log(response);
-  // return response.json();
-});
-// .then (function (data){
-//     console.log('Fetch Response \n-------------');
-//     console.log(data);
-// });
-=======
-var requestUrl = 'https://syndication.api.eb.com/production/article/1/xml';
-var requestUrl2 = 'https://images-api.nasa.gov/&appid=kSgH3akh7Qaqauv7AbKZyRc6pQjxcfVEOlObfDDl';
-var requestUrl3 = 
-<<<<<<< HEAD
-"https://syndication.api.eb.com/production/articles?articleTypeId=31&lastUpdated=2022-10-01";
-=======
 "https://syndication.api.eb.com/production/application/metadata";
->>>>>>> ccdba59d9719803433f0a7d7da31e632704ccd9c
 var requestOptions=  {
     method: 'GET',
     headers: {
@@ -46,7 +22,6 @@ fetch(requestUrl3, requestOptions)
     //     console.log('Fetch Response \n-------------');
     //     console.log(data);
     // });
->>>>>>> b0f138037198a24d6c200f879b453174d6462eb0
 
 // fetch(requestUrl2)
 // .then(function (response){
@@ -57,20 +32,7 @@ fetch(requestUrl3, requestOptions)
 //     console.log(data);
 // });
 
-<<<<<<< HEAD
 $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));
-=======
-    // fetch(requestUrl2)
-    // .then(function (response){
-    //     return response.json();
-    // })
-    // .then (function (data){
-    //     console.log('Fetch Response \n-------------');
-    //     console.log(data);
-    // });
 
 
 
-
-
->>>>>>> b0f138037198a24d6c200f879b453174d6462eb0
