91b1ae9ee3d9b4378c540b6f5127461f63411fad
Derek Meduri <derekmeduri@gmail.com>
2023-06-21 15:36:43 -0400

    linking background

--- assets/js/britannica.js
+++ assets/js/britannica.js
@@ -4,11 +4,14 @@ function wikiSearch(searchInput) {
   $("#searchBtn").on("click", function (event) {
     var searchInput = $("#user-input").val().trim();
     var url =
-      "https://en.wikipedia.org/w/api.php?action=query&prop=categories&list=search&titles=" +
+      "https://en.wikipedia.org/w/api.php?action=query&prop=revisions&titles=" +
       searchInput +
-      "&format=json&srsearch=" +
-      searchInput +
-      "&origin=*";
+      "&rvslots=*&rvprop=content&formatversion=2";
+    //   "https://en.wikipedia.org/w/api.php?action=query&prop=categories&list=search&titles=" +
+    //   searchInput +
+    //   "&format=json&srsearch=" +
+    //   searchInput +
+    //   "&origin=*";
 
     fetch(url)
       .then(function (response) {
@@ -17,16 +20,16 @@ function wikiSearch(searchInput) {
       //.then(function(response) {console.log(response);})
       //.then(function(data){return data.innerText;})
       .then(function (data) {
-        console.log(data.query.search[0].snippet);
+        // console.log(data.query.search[0].snippet);
         console.log(data);
         $("#articleSearch").empty();
-        var articleResponses = data.query.search[0].snippet;
-        console.log(articleResponses);
+        //var articleResponses = data.query.search[0].snippet;
+        //console.log(articleResponses);
         var pEl = document.createElement("p");
-        pEl.innerHTML = JSON.stringify(articleResponses).replace(
-          '<span class="searchmatch">',
-          ""
-        );
+        // pEl.innerHTML = JSON.stringify(articleResponses).replace(
+        //   '<span class="searchmatch">',
+        //   ""
+        // );
 
         class CustomReplacer {
           constructor(value) {
