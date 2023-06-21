8b930d7f3953b1c6734a76923585c40d2e1c14b9
booopyhij <mphillips2017@gmail.comgit config --global user.name booopyhijgit config --global user.email mphillips2017@gmail.com>
2023-06-21 02:55:34 -0400

    got rid of the britannica api and added the wiki api and got it working

--- assets/js/britannica.js
+++ assets/js/britannica.js
@@ -1,81 +1,31 @@
-//is this needed?
-// $(document).ready(function () {
-// $("#searchBtn").on("click", function (event) {
-//     event.preventDefault();
-//     
-//     searchInput = $("#user-input").val().trim();
-//     console.log(searchInput);
-//     britannicaSearch(searchInput);
-//  });
-// });
 
-function britannicaSearch(searchInput){
+     wikiSearch();
+     var divEl = document.getElementById('articleSearch');
+function wikiSearch(searchInput){
     $('#searchBtn').on('click', function(event){
     var searchInput = $('#user-input').val().trim();
-    var requestUrl2 = 'https://syndication.api.eb.com/production/article/' + searchInput + '/xml';
-    fetch(requestUrl2, requestOptions)
-    .then(function(response){
-        console.log(response);
-     fetch(requestUrl3, requestOptions)
-        .then(function (response){
-            console.log(response);
-            return response.text();
-        })
-         .then (function (data){
-             console.log('Fetch Response \n-------------');
-             console.log(data);
-             return data.innerText;
-         }).then(function (articleResponses) { 
-        
-        $('#articleSearch').empty();
-        // fix the for loop
-        var articleResponses = data.articles[i].title[i];
-        for (let i=0; i < articleResponses.length; i++){
-            //figure out why this isn't reading the object
-        console.log(articleResponses);
-        return data.innerText;}
-        // if statement needed?   
-        var userArticle = $('<p>' + articleResponses + '</p>');
-        console.log(userArticle);
-        // submit listener here
-        // make sure the appending works properly
-       divEl.append(userArticle);
-})})})};
-    
-    var divEl = document.getElementById('articleSearch');
-    
-    var requestUrl = "https://syndication.api.eb.com/production/article/781/xml";
-    var requestUrl3 =
-    "https://syndication.api.eb.com/production/articles?articleTypeId=1&categoryId=2";
-    var requestOptions=  {
-        method: 'GET',
-        headers: {
-            'x-api-key': '899b3b09-5634-4fd8-8c68-5cdc598467a3',
-            accept: '*/*',
-        },
-    };
-    fetch(requestUrl3, requestOptions)
-    .then(function (response){
-        console.log(response);
-        return response.text();
-    })
-     .then (function (data){
-         console.log('Fetch Response \n-------------');
-         console.log(data);
-         return data.innerText;
-     });
+    var url = 'https://en.wikipedia.org/w/api.php?action=query&prop=categories&list=search&titles='+ searchInput + '&format=json&srsearch=' +searchInput + '&origin=*' ;
 
-    fetch(requestUrl, requestOptions)
-     .then(function(response){
-        console.log(response);
-        return response.text();
-     })
-     .then (function (data){
-        console.log('Fetch Response \n-------------');
+fetch(url)
+    .then(function(response){return response.json();})
+    //.then(function(response) {console.log(response);})
+    //.then(function(data){return data.innerText;})
+    .then(function(data) {console.log(data.query.search[0].snippet);
         console.log(data);
-        return data.innerText;
-     });
-     //.then(response => divEl.innerHTML = response);
-
-     britannicaSearch();
-     
+        $('#articleSearch').empty();
+        var articleResponses = data.query.search[0].snippet;
+        console.log(articleResponses);
+        var pEl = document.createElement('p');
+        pEl.textContent = articleResponses;
+        //pEl.innerText = JSON.stringify(data.query.search[0].snippet);
+         //var newPEl = pEl.toString().replace(/<span class=\"searchmatch">/g, '');
+        // newPEl.textContent = pEl;
+        // var sel = document.getElementsByTagName('<p>').innerHTML;
+        // console.log(sel);
+        // var redo = newPEl + sel;
+        // console.log(redo);
+        // // var userArticle = $('<p>' + JSON.stringify(articleResponses) + '</p>');
+        // console.log(userArticle);
+        divEl.append(pEl);
+    });   
+})};
